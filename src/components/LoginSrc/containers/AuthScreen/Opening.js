6d2a93162760582cf4128f1ee8c9630665a2c9f8
firmangel8 <firmangel8@gmail.com>
2018-12-06 06:51:52 +0800

    revised some changes in opening form

--- src/components/LoginSrc/containers/AuthScreen/Opening.js
+++ src/components/LoginSrc/containers/AuthScreen/Opening.js
@@ -15,17 +15,17 @@ export default class Opening extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <View animation={'zoomIn'} delay={600} duration={400}>
+        {/* <View animation={'zoomIn'} delay={600} duration={400}>
           <CustomButton
             text={'Create Account'}
             onPress={this.props.onCreateAccountPress}
             buttonStyle={styles.createAccountButton}
             textStyle={styles.createAccountButtonText}
           />
-        </View>
+        </View> */}
         <View style={styles.separatorContainer} animation={'zoomIn'} delay={700} duration={400}>
           <View style={styles.separatorLine} />
-          <Text style={styles.separatorOr}>{'Or'}</Text>
+          <Text style={styles.separatorOr}>{'Press Sign In'}</Text>
           <View style={styles.separatorLine} />
         </View>
         <View animation={'zoomIn'} delay={800} duration={400}>
